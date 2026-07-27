d0d679724e9e952c7dbcee8cadb7239a55ddf531
85desapego <renatabarbosa43@gmail.com>
2026-07-27 18:19:10 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-if('serviceWorker' in navigator){navigator.serviceWorker.register('sw.js');}
+
+if('serviceWorker' in navigator){navigator.serviceWorker.register('sw.js')}
 ultimo.value=localStorage.getItem('ultimoCodigo')||100;
 function gerar(){
-const n=Number(ultimo.value)+1;
-ultimo.value=n;
-localStorage.setItem('ultimoCodigo',n);
-const sku='85-'+String(n).padStart(6,'0');
-cat.textContent=categoria.value;
-tamanho.textContent=tam.value;
-valor.textContent=preco.value;
-document.getElementById('sku').textContent=sku;
-JsBarcode('#barcode',sku,{format:'CODE128',displayValue:false,height:40,width:1.6,margin:2});
-preview.classList.remove('hide');
+ let ult=Number(ultimo.value);
+ let html='';
+ for(let i=0;i<Number(quantidade.value);i++){
+   ult++;
+   const sku='85-'+String(ult).padStart(6,'0');
+   html+=`<div class="et">
+   <div class="logo">85 DESAPEGO</div>
+   <div class="info"><span>${categoria.value}</span><span>Tam ${tam.value}</span></div>
+   <div class="preco">R$ ${preco.value}</div>
+   <svg id="bc${i}"></svg>
+   <div class="sku">${sku}</div>
+   </div>`;
+ }
+ ultimo.value=ult;
+ localStorage.setItem('ultimoCodigo',ult);
+ etiquetas.innerHTML=html;
+ let start=ult-Number(quantidade.value)+1;
+ for(let i=0;i<Number(quantidade.value);i++){
+   let sku='85-'+String(start+i).padStart(6,'0');
+   JsBarcode('#bc'+i,sku,{format:'CODE128',displayValue:false,height:40,width:1.5,margin:2});
+ }
 }
